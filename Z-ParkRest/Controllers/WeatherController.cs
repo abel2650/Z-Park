0baf9e5e785b2ef9f262a01b1e3f338adfe9a01b
Xevoz_ <Niklas.loev@gmail.com>
2024-12-10 13:21:15 +0100

    retskrivning

--- Z-ParkRest/Controllers/WeatherController.cs
+++ Z-ParkRest/Controllers/WeatherController.cs
@@ -1,11 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Z_ParkLib;
-using Z_ParkLib.Model;
-using Z_ParkLib.repositories;
-using Z_ParkRest.Model;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace Z_ParkRest.Controllers;
 
@@ -20,38 +13,36 @@ public class WeatherController : ControllerBase
         {
             string apiKey = "f12adcb1-0bcb-4d7b-ad45-b8475b0f12fd"; // API key til ForecastDataAPI (personlig)
             string stationId = "06170";     // Replace with the station ID for Roskilde Lufthavn
-            //Bruges som "Endpoint" til at connecte til DMI's URL.
+            //Bruges som "Endpoint" til at connected til DMIs URL.
             string endpoint = $"https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?stationId={stationId}&api-key={apiKey}";
 
             //Anbefalet metoder til at indhente API'er.
-            using (HttpClient client = new HttpClient())
+            using HttpClient client = new HttpClient();
+            try
             {
-                try
-                {
-                    //Sender en HTTP GET-anmodning til definere Endpoint. Vores endpoint fra før (DMI's. hjemmeside)
-                    //HttpResponseMessage venter på Objekt response .Result blokere tråden indtil response fuldført.
-                    HttpResponseMessage response = client.GetAsync(endpoint).Result;
+                //Sender en HTTP GET-anmodning til definere Endpoint. Vores endpoint fra før (DMIs. Hjemmeside)
+                //HttpResponseMessage venter på Objekt response.Result blokere tråden indtil response fuldført.
+                HttpResponseMessage response = client.GetAsync(endpoint).Result;
 
-                    //kontrollere tatuskoden, hvos mellem 200-299 = succes
-                    if (response.IsSuccessStatusCode)
-                    {
-                        //Læser indholdet af svaret som en tekststreng.
-                        string responseData = response.Content.ReadAsStringAsync().Result;
-                        Console.WriteLine($"Her er Dataen: {responseData}");
-                    }
-                    else
-                    {
-                        //fejlmeddelelse 
-                        Console.WriteLine($"Fejl: {response.StatusCode}");
-                    }
-                    
-                }
-                
-                //fejlmeddelelse 
-                catch (Exception ex)
+                //kontrollere statuskoden, hvor mellem 200-299 = succes
+                if (response.IsSuccessStatusCode)
                 {
-                    Console.WriteLine($"Fejl: {ex.Message}");
+                    //Læser indholdet af svaret som en tekststreng.
+                    string responseData = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine($"Her er Data: {responseData}");
                 }
+                else
+                {
+                    //fejlmeddelelse 
+                    Console.WriteLine($"Fejl: {response.StatusCode}");
+                }
+                    
+            }
+                
+            //fejlmeddelelse 
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fejl: {ex.Message}");
             }
         }
     }
